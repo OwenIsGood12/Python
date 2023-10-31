aad77d5ee583a5b927aed0f876d4c677c317913a
OwenIsGood12 <149517164+OwenIsGood12@users.noreply.github.com>
2023-10-31 12:44:34 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-print('test')
+import random
+
+number = random.randint(1, 100)
+guessing = True
+while guessing:
+  guess = input('Guess the number')
+  if int(guess) > number:
+    print('The number is less than', guess)
+  if int(guess) < number:
+    print('The number is greater than', guess)
+  if int(guess) = number:
+    print('Correct!'
+    guessing = False
